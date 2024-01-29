7cc82ff48f0ae730136e10ee89c8a95714361342
ziko144 <zakirkazimzad19@gmail.com>
2024-01-29 18:41:43 +0400

    add

---
+++ app.js
@@ -0,0 +1,7 @@
+var typed = new Typed((".skills"),{
+    strings : ["Frontend developer","Web-Developer","Web Designer"],
+    typeSpeed : 100,
+    backSpeed : 100,
+    backDelay : 1000,
+    loop : true
+})
